b5635138dd51ce8053787a9c3fd73b2849b06a2e
siddiqa <siddiqaamir512@gmail.com>
2025-06-26 16:21:40 +0500

    Updated layout file

--- app/_layout.js
+++ app/_layout.js
@@ -9,26 +9,26 @@ const _layout = () => {
   return (
     <View style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="splash" /> */}
-        {/* <Stack.Screen name="onboarding-a" /> */}
-        {/* <Stack.Screen name="onboarding-b" /> */}
-        {/* <Stack.Screen name="onboarding-c" /> */}
-        {/* <Stack.Screen name="onboarding-d" /> */}
-        {/* <Stack.Screen name="account-entry" /> */}
-        {/* <Stack.Screen name="login" /> */}
-        {/* <Stack.Screen name="password-forget" /> */}
-        {/* <Stack.Screen name="password-reset" /> */}
-        {/* <Stack.Screen name="register" /> */}
-        {/* <Stack.Screen name="otp" /> */}
-        {/* <Stack.Screen name="verify" /> */}
+        <Stack.Screen name="splash" />
+        <Stack.Screen name="onboarding-a" />
+        <Stack.Screen name="onboarding-b" />
+        <Stack.Screen name="onboarding-c" />
+        <Stack.Screen name="onboarding-d" />
+        <Stack.Screen name="account-entry" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="password-forget" />
+        <Stack.Screen name="password-reset" />
+        <Stack.Screen name="register" />
+        <Stack.Screen name="otp" />
+        <Stack.Screen name="verify" />
         <Stack.Screen name="home" />
-        {/* <Stack.Screen name="schedule-visit" /> */}
-        {/* <Stack.Screen name="property-details" /> */}
-        {/* <Stack.Screen name="booking" /> */}
-        {/* <Stack.Screen name="checkout" /> */}
-        {/* <Stack.Screen name="payment" /> */}
-        {/* <Stack.Screen name="payment-success" /> */}
-        {/* <Stack.Screen name="home-page2" /> */}
+        <Stack.Screen name="schedule-visit" />
+        <Stack.Screen name="property-details" />
+        <Stack.Screen name="booking" />
+        <Stack.Screen name="checkout" />
+        <Stack.Screen name="payment" />
+        <Stack.Screen name="payment-success" />
+        <Stack.Screen name="home-page2" />
       </Stack>
     </View>
   );
