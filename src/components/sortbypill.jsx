ced739f20ee8a494cc9880e0beebc2153a91f3a0
siddiqa <siddiqaamir512@gmail.com>
2025-07-07 17:22:03 +0500

    Created a separate pill sort items

---
+++ src/components/sortbypill.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles/global";
+import { View, Text, TouchableOpacity } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+
+export default function SortbyPill(props) {
+  const { openSortBottomSheet } = props;
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.categoryButton}
+        onPress={openSortBottomSheet}
+      >
+        <Text style={styles.categoryButtonText}>Sort by</Text>
+        <Entypo name="chevron-small-down" size={24} color="gray" />
+      </TouchableOpacity>
+    </View>
+  );
+}
