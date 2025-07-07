4202eeb22c2efac00c358f05681b5b414da40763
siddiqa <siddiqaamir512@gmail.com>
2025-07-07 17:47:36 +0500

    Added bottomsheets to home page

--- app/home.jsx
+++ app/home.jsx
@@ -1,25 +1,69 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "../src/styles/global";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { View, ScrollView } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
-import HomeHeader from '../src/components/homeheader';
-import RecommendedHostels from '../src/components/recommendedhostels';
-import ViewedHostels from '../src/components/viewedhostels';
-import TabIcons from '../src/components/tabicons';
-import { useRouter } from "expo-router";
+import HomeHeader from "../src/components/homeheader";
+import RecommendedHostels from "../src/components/recommendedhostels";
+import ViewedHostels from "../src/components/viewedhostels";
+import TabIcons from "../src/components/tabicons";
+import ScheduleBottomSheet from "../src/components/schedulebottomsheet";
+import SortbyBottomSheet from "../src/components/sortbybottomsheet";
+import GenderBottomSheet from "../src/components/genderbottomsheet";
 
-export default function Home() {
-  const router = useRouter();
+const Home = () => {
+  const bottomSheetRef = useRef(null);
+  const openBottomSheet = () => {
+    bottomSheetRef.current?.snapToIndex(3);
+  };
+  const closeBottomSheet = () => {
+    bottomSheetRef.current?.snapToIndex(-1);
+  };
+
+  const sortBottomSheetRef = useRef(null);
+  const openSortBottomSheet = () => {
+    sortBottomSheetRef.current?.snapToIndex(2);
+  };
+  const closeSortBottomSheet = () => {
+    sortBottomSheetRef.current?.snapToIndex(-1);
+  };
+
+  const genderBottomSheetRef = useRef(null);
+  const openGenderBottomSheet = () => {
+    genderBottomSheetRef.current?.snapToIndex(2);
+  };
+  const closeGenderBottomSheet = () => {
+    genderBottomSheetRef.current?.snapToIndex(-1);
+  };
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ScrollView>
-      <View style={styles.container}>
-        <HomeHeader />
-        <RecommendedHostels />
-        <ViewedHostels />
-      </View>
-      </ScrollView>
+      <GestureHandlerRootView style={styles.container}>
+        <ScrollView>
+          <View style={styles.container}>
+            <HomeHeader
+              openGenderBottomSheet={openGenderBottomSheet}
+              openSortBottomSheet={openSortBottomSheet}
+            />
+            <RecommendedHostels openBottomSheet={openBottomSheet} />
+            <ViewedHostels />
+          </View>
+        </ScrollView>
+        <ScheduleBottomSheet
+          bottomSheetRef={bottomSheetRef}
+          closeBottomSheet={closeBottomSheet}
+        />
+        <GenderBottomSheet
+          bottomSheetRef={genderBottomSheetRef}
+          closeBottomSheet={closeGenderBottomSheet}
+        />
+        <SortbyBottomSheet
+          bottomSheetRef={sortBottomSheetRef}
+          closeBottomSheet={closeSortBottomSheet}
+        />
+      </GestureHandlerRootView>
       <TabIcons />
     </SafeAreaView>
   );
-}
+};
+export default Home;
