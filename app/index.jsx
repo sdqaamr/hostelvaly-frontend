7169ba06ee1303d09e5937979bd7562877b79560
siddiqa <siddiqaamir512@gmail.com>
2025-07-07 17:15:29 +0500

    Added GestureHandlerRootView to index file

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Splash from "./splash";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
- function App() {
-  
+function App() {
   return (
     <SafeAreaProvider>
-      <Splash />
+      <GestureHandlerRootView style={{ flex: 1 }}></GestureHandlerRootView>
     </SafeAreaProvider>
   );
-};
+}
 export default App;
 
 // export { default } from 'expo-router';
