cae9879334ebca0a462f9e39f7f51d7925c83ccc
siddiqa <siddiqaamir512@gmail.com>
2025-07-08 19:14:21 +0500

    Removed extra code from genderpill.jsx .

--- src/components/genderpill.jsx
+++ src/components/genderpill.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 import styles from "../styles/global";
-import { View, Text, TouchableOpacity } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import Feather from "@expo/vector-icons/Feather";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Text, TouchableOpacity } from "react-native";
 import Entypo from "@expo/vector-icons/Entypo";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import LogoText from "./logotext";
-import { Modal } from "react-native";
-import { useState } from "react";
 
 export default function GenderPill(props) {
   const { openGenderBottomSheet } = props;
