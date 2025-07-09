7f43fb6c415fb713be9d20192dd04cc2208e7de0
siddiqa <siddiqaamir512@gmail.com>
2025-07-09 17:58:30 +0500

    Added status bar to layout file

---
+++ app/_layout.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import useCustomFonts from "../src/components/usecustomfonts";
+import { StatusBar } from "react-native";
+
+const _layout = () => {
+  useCustomFonts();
+  console.log("App loaded");
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={["left", "right"]}>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="#F0F0F0"
+      />
+      <Stack screenOptions={{ headerShown: false, statusBarTranslucent: true }}>
+        <Stack.Screen name="splash" />
+        <Stack.Screen name="onboarding" />
+        <Stack.Screen name="account-entry" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="password-forget" />
+        <Stack.Screen name="password-reset" />
+        <Stack.Screen name="register" />
+        <Stack.Screen name="otp" />
+        <Stack.Screen name="verify" />
+        <Stack.Screen name="tabs" />
+        <Stack.Screen name="schedule-visit" />
+        <Stack.Screen name="property-details" />
+        <Stack.Screen name="booking" />
+        <Stack.Screen name="checkout" />
+        <Stack.Screen name="payment" />
+        <Stack.Screen name="payment-success" />
+        <Stack.Screen name="home-page2" />
+      </Stack>
+    </SafeAreaView>
+  );
+};
+export default _layout;
