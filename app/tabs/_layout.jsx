1b59897ffe1775a2010895cb7a60aee18095f1fe
siddiqa <siddiqaamir512@gmail.com>
2025-07-09 18:11:05 +0500

    Created navigation tabs for home screeen

---
+++ app/tabs/_layout.jsx
@@ -0,0 +1,58 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Platform } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#8100d1",
+        tabBarInactiveTintColor: "#ce80ff",
+        tabBarLabelStyle: { fontSize: 11, fontWeight: "bold" },
+        tabBarStyle: {
+          backgroundColor: "white",
+          maxHeight: 80,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          tabBarLabel: "Messages",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="message1" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="likes"
+        options={{
+          tabBarLabel: "Likes",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="hearto" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="contacts"
+        options={{
+          tabBarLabel: "Contacts",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="contacts" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/tabs/contacts.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../../src/styles/global";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+
+export default function Verify() {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.mainTitle}>
+          Contacts!
+        </Text>
+        {/* <Text style={styles.verifyText}>Your account has been verified successfully</Text>
+        <Text></Text> */}
+      </View>
+    </SafeAreaView>
+  );
+}
